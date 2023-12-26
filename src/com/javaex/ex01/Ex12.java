216f4d6d9475abcd2c175cab8168364ed4408107
clz2024.red <clz2024.red@gmail.com>
2023-12-26 12:53:21 +0900

    1st

---
+++ src/com/javaex/ex01/Ex12.java
@@ -0,0 +1,26 @@
+package com.javaex.ex01;
+
+public class Ex12 {
+
+	public static void main(String[] args) {
+
+		//관계 연산자
+		int n1 = 5;
+		int n2 = 3;
+		
+		boolean result = n1<n2;     //f
+		//System.out.println(result);
+		
+		System.out.println(n1<n2);  //5<3  f
+		System.out.println(n1>n2);  //5>3  t
+		
+		System.out.println(n1<=n2);  //5<=3  f
+		System.out.println(n1>=n2);  //5>=3  t
+		System.out.println(3>=n2);  //3>=3   t
+		
+		System.out.println(n1==n2);  //5==3   f
+		System.out.println(n1!=n2);  //5!=3   t
+		
+	}
+
+}
