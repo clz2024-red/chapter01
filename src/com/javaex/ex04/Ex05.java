d2e23203ab72288482f73ea392848be5172ac7a2
clz2024.red <clz2024.red@gmail.com>
2023-12-28 15:03:03 +0900

    반복문

---
+++ src/com/javaex/ex04/Ex05.java
@@ -0,0 +1,19 @@
+package com.javaex.ex04;
+
+public class Ex05 {
+
+	public static void main(String[] args) {
+
+		int sum = 0;
+		int max = 1256;
+		for(int i=1; i<=max; i++) {
+			sum = sum + i;
+			System.out.println(i+"까지의 합은 " + sum);
+		}
+		
+		System.out.println("1부터" + max + "까지의 합은" + sum);
+		
+		
+	}
+
+}
---
+++ src/com/javaex/ex04/Ex08.java
@@ -0,0 +1,29 @@
+package com.javaex.ex04;
+
+public class Ex08 {
+
+	public static void main(String[] args) {
+		
+		//세로
+		for(int y=1; y<=6; y++) {
+			//System.out.println("*");
+			
+			//가로
+			for(int x=1; x<=y; x++) {
+				System.out.print("*");				
+			}
+			
+			//줄바꿈
+			System.out.println("");
+			
+		}
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
